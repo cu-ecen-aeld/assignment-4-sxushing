7932804823564fef8b24afc9bc668acc95449090
sxushing <dagonzai87@gmail.com>
2023-05-31 12:01:16 +0800

    Add

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 845e6a26e95f402e714d4d617d36eed4b1143e38
+AESD_ASSIGNMENTS_VERSION =78959496464959
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
